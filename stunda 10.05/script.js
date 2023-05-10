c09fdb5626bdac62e603f71de5fed312ca539323
valainea <114050586+valainea@users.noreply.github.com>
2023-05-10 09:35:43 +0300

    Add files via upload

---
+++ stunda 10.05/script.js
@@ -0,0 +1,5 @@
+const coolButton = document.querySelector('.js-cool');
+
+coolButton.addEventListener('click', () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
